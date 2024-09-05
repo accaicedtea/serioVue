3e1492f1822530658306e0124013416054c13390
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2024-09-05 19:19:13 +0200

    task da correggere da errore

---
+++ src/pages/admin/task.vue
@@ -0,0 +1,248 @@
+<template>
+  <v-data-table :headers="headers" :items="desserts" :sort-by="[{ key: 'name', order: 'asc' }]">
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Task</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ props }">
+            <v-btn class="mb-2" color="primary" dark v-bind="props">
+              Nuova attività
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col >
+                    <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
+                  </v-col>
+                  <v-col >
+                    <v-text-field v-model="editedItem.category" label="Categoria"></v-text-field>
+                  </v-col>
+                    <v-col>
+                    <v-text-field v-model="editedItem.stato" label="stato" :value="0"></v-text-field>
+                    </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="close">
+                Cancel
+              </v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="save">
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon class="me-2" size="small" @click="editItem(item)">
+        mdi-pencil
+      </v-icon>
+      <v-icon size="small" @click="deleteItem(item)">
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+import { supabase } from '../../plugins/supabase';
+
+export default {
+  name: 'Task',
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        title: 'Name',
+        align: 'start',
+        sortable: false,
+        key: 'name',
+      },
+      { title: 'Categoria', key: 'categoria' },
+      { title: 'Stato', key: 'stato' },
+      { title: 'Actions', key: 'actions', sortable: false },
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: '',
+      categories: '',
+      stato: 0,
+    },
+    defaultItem: {
+      name: '',
+      categories: '',
+      stato: 0,
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+    dialogDelete(val) {
+      val || this.closeDelete()
+    },
+  },
+
+  created() {
+    this.initialize()
+  },
+
+  methods: {
+    initialize() {
+      this.desserts = [
+        {
+          name: 'Frozen Yogurt',
+          categoria: 'Dessert',
+          stato: true,
+        },
+        {
+          name: 'Ice cream sandwich',
+          categoria: 'Dessert',
+          stato: true,
+        },
+        {
+          name: 'Eclair',
+          categoria: 'Dessert',
+          stato: true,
+        },
+        {
+          name: 'Cupcake',
+          categoria: 'Dessert',
+          stato: true,
+        },
+        {
+          name: 'Gingerbread',
+          categoria: 'Dessert',
+          stato: true,
+        },
+        {
+          name: 'Jelly bean',
+          categoria: 'Caramelle',
+          stato: true,
+        },
+        {
+          name: 'Lollipop',
+          categoria: 'Caramelle',
+          stato: true,
+        },
+        {
+          name: 'Honeycomb',
+          categoria: 'Dessert',
+          stato: true,
+        },
+        {
+          name: 'Donut',
+          categoria: 'Dessert',
+          stato: true,
+        },
+        {
+          name: 'KitKat',
+          categoria: 'Cioccolato',
+          stato: true,
+        },
+
+      ]
+    },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete() {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        if (this.editedItem.name && this.editedItem.category) {
+          this.desserts.push(this.editedItem);
+          supabase
+            .from('task')
+            .insert([
+              {
+                name: this.editedItem.name,
+                categoria: parseInt(this.editedItem.category),
+                status: this.editedItem.stato
+              }
+            ])
+            .then(response => {
+              console.log('New task created:', response.data[0]);
+            })
+            .catch(error => {
+              console.error('Error creating task:', error);
+            });
+        }
+      }
+      this.close()
+    },
+  },
+}
+</script>
+
+<style scoped>
+/* Your component's CSS styles go here */
+</style>
